03f114dd3f8f583ea4b4a04ce81a35291f03fe36
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2023-05-15 13:07:51 +0200

    tryparse

---
+++ TryparseKomplettering/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int age = 0;
+bool question = false;
+
+while (question != true)
+{
+  Console.WriteLine("Din ålder:");
+  string input = Console.ReadLine();
+
+  // int age = int.Parse(input);
+
+  question = int.TryParse(input, out age);
+}
+
+if (question == true)
+{
+  if (age < 21)
+  {
+    Console.WriteLine("Du får inte köpa alkohol på systemet.");
+  }
+  else
+  {
+    Console.WriteLine("Du får köpa alkohol på systemet.");
+  }
+}
+
+
+Console.ReadLine();
+
